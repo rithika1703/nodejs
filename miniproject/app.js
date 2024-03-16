fabfba9896e94efe111d1609e067244de14a386e
rithika1703 <rithika17032001>
2024-03-16 16:05:17 +0530

    updated

--- miniproject/app.js
+++ miniproject/app.js
@@ -42,5 +42,10 @@ const server =  http.createServer((req, res) =>{
         </html>`;
         res.writeHead(200,{'Content-Type':'text/html'});
         res.end(htmlContent);
-    })
-})
+    });
+
+});
+
+server.listen(3000,() =>{
+    console.log('Server is running on port 3000');
+});
