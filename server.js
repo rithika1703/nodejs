222a7b9864d41aeaa545de73404876984cfb94dc
rithika1703 <rithika17032001>
2024-03-16 14:36:53 +0530

    hi there

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    console.log(req.url, req.method,req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write()
+})
