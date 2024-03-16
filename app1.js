f54947199735e89cf232b915a0572b34ec94ca48
rithika1703 <rithika17032001>
2024-03-16 16:59:54 +0530

    changes

---
+++ app1.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, Rithika!');
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 4000');
+});
