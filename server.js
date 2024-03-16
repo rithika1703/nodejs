abb6594cd71471b3a38fa47964515ff07a6ee9c0
rithika1703 <rithika17032001>
2024-03-16 14:43:49 +0530

    changes made

--- server.js
+++ server.js
@@ -3,5 +3,16 @@ const http = require('http');
 const server = http.createServer((req, res) =>{
     console.log(req.url, req.method,req.headers);
     res.setHeader('Content-Type', 'text/html');
-    res.write()
+    if(url === '/home'){
+        res.end('welcome home');
+    }else if(url ==='about'){
+        res.end('welcome to About Us page')
+    }else if(url === 'node'){
+        res.end('welcom to my Node.js project');
+    }
+});
+
+server.listen(4000,() =>{
+    console.log('server is running on port 4000');
 })
+
