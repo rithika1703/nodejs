b0ecd799fdc13dccbf4f34a495534b48b1bde693
rithika1703 <rithika17032001>
2024-03-16 15:58:41 +0530

    changes made

--- miniproject/app.js
+++ miniproject/app.js
@@ -10,3 +10,26 @@ function readMessages(callback) {
         callback(null, messages);
     });
 }
+function updateMessages(newMessage, callback) {
+    fs.appendFile('messages.txt', newMessage + '\n', 'utf8', (err) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null);
+    });
+}
+const server =  http.createServer((req, res) =>{
+    readMessages((err,message) =>{
+        if(err){
+            res.writeHead(500,{'Content-Type' : 'text/plain'});
+            res.end('Internal Server Error');
+            return ;
+        }
+        let htmlContent =`
+        <html>
+        <head><title> Messages </title></head>
+        <body> <h1> Messages </h1></body>
+        `
+    })
+})
