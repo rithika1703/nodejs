c52c87d449140ee5aaa244e3c36b96a082a1ef40
rithika1703 <rithika17032001>
2024-03-21 12:59:52 +0530

    changes

--- app-m.js
+++ app-m.js
@@ -5,13 +5,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res,  next) => {
-    console.log('In the Middleware!');
-    next();
+app.use('/',(req,res,next) => {
+    console.log('This always runs');
 });
 
-app.use((req, res,  next) => {
+app.use('/add-product',(req, res,  next) => {
     console.log('In another Middleware!');
+    res.send('<h1>the add product page!</h1>');
+});
+
+
+
+app.use('/',(req, res,  next) => {
+    console.log('In another Middleware!');
+    res.send('<h1>hello from Express!</h1>');
 });
 
 const server = http.createServer(app);
