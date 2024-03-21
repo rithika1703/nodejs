8ba5945654b08b0a76890dbdcdfdd2d806c36f5d
rithika1703 <rithika17032001>
2024-03-21 13:00:39 +0530

    changes

--- app-a.js
+++ app-a.js
@@ -4,3 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/add-product', (req, res) => {
+    res.send(`
+        <form action="/add-product" method="POST">
+            <input type="text" name="productName" placeholder="Product Name">
+            <input type="text" name="productSize" placeholder="Product Size"> <!-- New input for product size -->
+            <button type="submit">Add Product</button>
+        </form>
+    `);
+});
+
