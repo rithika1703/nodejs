80f6544e024b5628a4bd50fb5fee348a80ca7109
rithika1703 <rithika17032001>
2024-03-24 12:18:42 +0530

    changes made

--- app-m.js
+++ app-m.js
@@ -1,7 +1,6 @@
 
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -12,6 +11,12 @@ const shopRoutes = require('././routes/shop');
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req , res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 
 server.listen(3000);
 
