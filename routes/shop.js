981dc037c41370c8a21b8412698d31c166b215fd
rithika1703 <rithika17032001>
2024-03-24 12:27:59 +0530

    changes made

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/',(req,res,next)=> {
-    res.send('<h1>Hello from Express!<h1>');
-})
+router.get('/add-product',(req, res,  next) => {
+    res.send('<form action="/product" method="POST"><input type="text"name="title"><button type="submit">');
+});
+
+
+
+router.post('/add-product',(req, res,  next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
 module.exports = router;
